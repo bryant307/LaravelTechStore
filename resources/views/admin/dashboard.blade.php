fab2b9e9af9c1fc406d83828a8c67c4854de20e3
bryant307 <torresb307@gmail.com>
2025-05-17 01:55:25 -0600

    Se cambio el idioma de la aplicación a español Se agregaron los archivos de idioma en español Se agrega la funcionalidad de familias Se puede crear, editar y eliminar familias Se pueede cambiar foto de perfil en el dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,46 @@
-<x-admin-layout>
+{{-- plantilla del dashboard --}}
+{{-- Se agrega el breadcrumb para el diseño de la vista --}}
+{{-- Diseño del breadcrumb --}}
 
-    <h1>Hola </h1>
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'route' => route('admin.dashboard'),
+    ]
+]">
+
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+
+        <div class="bg-white rounded-lg shadow-lg p-6">
+            <div class="flex items-center">
+                <img class="size-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                    alt="{{ Auth::user()->name }}" />
+
+                <div class="ml-4 flex-1">
+                    <h2 class="text-lg font-semibold">
+                        Bienvenido, {{ Auth::user()->name }}
+                    </h2>
+
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="text-sm hover:text-blue-500">
+                            Cerrar sesión
+                        </button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-lg p-6 flex items-center justify-center">
+
+            <h2 class="text-xl font-semibold">
+                Laravel TechStore
+
+            </h2>
+
+        </div>
+
+    </div>
+
 </x-admin-layout>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,3 +1,5 @@
+@props(['breadcrumbs' => []])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -12,6 +14,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    {{-- FontAwesome --}}
+    <script src="https://kit.fontawesome.com/3e7eb58d7a.js" crossorigin="anonymous"></script>
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -19,20 +24,41 @@
     @livewireStyles
 </head>
 
-<body class="font-sans antialiased" x-data="{
+<body class="font-sans antialiased" 
+    x-data="{
     sidebarOpen: false
+    }"
+    :class="{
+        'overflow-y-hidden' : sidebarOpen
     }">
+
+    <div class="fixed inset-0 bg-gray-900 bg-opacity-50 z-20 sm:hidden"
+        style="display: none;"
+       x-show="sidebarOpen"
+       x-on:click="sidebarOpen = false">
+
+    </div>
 
     @include('layouts.partials.admin.sidebar')
     @include('layouts.partials.admin.navigation')
     
-
+{{-- Se agrega el breadcrumb para el diseño de la vista --}}
+    {{-- Diseño del breadcrumb --}}
     <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+
+        <div class="mt-14">
+            @include('layouts.partials.admin.breadcrumb')
+
+            <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 ">
             
+            
+
             {{ $slot }}
 
         </div>
+        </div>
+
+        
     </div>
 
 
